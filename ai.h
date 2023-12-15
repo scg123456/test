47b7a9bdeb5aefc528eb9f285f1dcc6cc8627ded
scg123456 <144107777+scg123456@users.noreply.github.com>
2023-12-16 00:22:20 +0800

    Add files via upload

---
+++ ai.h
@@ -0,0 +1,10 @@
+#pragma once
+const int black=1;
+const int white=2;
+int aibeside(int x, int y);
+int aibeside2(int x, int y);
+int aibeside3(int x, int y);
+int aibeside4(int x, int y);
+int corner(int x, int y);
+int attack(int x, int y);
+void ai();
